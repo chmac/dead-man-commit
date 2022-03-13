d6f448c306dbd50137aa40ea0d1c99ebf048dced
Callum Macdonald <git@callum-macdonald.com>
2022-03-13 19:56:35 +0100

    Build for 3 architectures.

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,16 @@
 THISDIR=`dirname "$0"`
 
 deno compile --allow-read --allow-run --allow-env \
+  --target x86_64-apple-darwin \
   --output "${THISDIR}/../build/dead-man-commit.x86_64-apple-darwin" \
+  "${THISDIR}/../mod.ts"
+
+deno compile --allow-read --allow-run --allow-env \
+  --target x86_64-unknown-linux-gnu \
+  --output "${THISDIR}/../build/dead-man-commit.x86_64-unknown-linux-gnu" \
+  "${THISDIR}/../mod.ts"
+  
+deno compile --allow-read --allow-run --allow-env \
+  --target aarch64-apple-darwin \
+  --output "${THISDIR}/../build/dead-man-commit.aarch64-apple-darwin" \
   "${THISDIR}/../mod.ts"
