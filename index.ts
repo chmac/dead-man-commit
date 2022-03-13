3e0b8b89b5532b2adf607f6a7127512b3e6c1d02
Callum Macdonald <git@callum-macdonald.com>
2022-03-13 15:22:42 +0100

    Simple multi git.

--- index.ts
+++ index.ts
@@ -5,13 +5,11 @@ const args = parse(Deno.args);
 
 const verbose = args.v || args.verbose || false;
 
-console.dir(args);
+const gitArgs = args._.map((arg) =>
+  typeof arg === "string" ? arg : arg.toString()
+);
 
-const positional = args._;
-
-if (positional[0] === "status") {
-  console.log("#T8r32X status");
-}
+const textDecoder = new TextDecoder();
 
 await log.setup({
   handlers: {
@@ -34,14 +32,37 @@ const getGitDirs = async (path: string) => {
       continue;
     }
 
-    const potentialGitPath = join(path, entry.name, `.git`);
+    const entryPath = join(path, entry.name);
+
+    const potentialGitPath = join(entryPath, `.git`);
     const exists = await doesDirectoryExist(potentialGitPath);
 
     if (!exists) {
       continue;
     }
 
-    console.log(`#xmgnE0 Found git repo ${potentialGitPath}`);
+    logger.debug(`#xmgnE0 Found git repo ${potentialGitPath}`);
+
+    const command = Deno.run({
+      cmd: ["git", "-C", entryPath, ...gitArgs],
+      stdout: "piped",
+      stderr: "piped",
+    });
+
+    const [status, stdout, stderr] = await Promise.all([
+      command.status(),
+      command.output(),
+      command.stderrOutput(),
+    ]);
+
+    if (!status.success) {
+      logger.error(`#8eKr7E Error running git command`);
+      logger.error(`Path was ${entryPath}`);
+      logger.error(textDecoder.decode(stderr));
+    }
+
+    logger.info(`#HkMvHN ${entryPath}`);
+    logger.info(textDecoder.decode(stdout));
   }
 };
 
@@ -51,7 +72,7 @@ const start = async () => {
 
 start()
   .then(() => {
-    console.log(`#VO2Jj4 Finished`);
+    logger.debug(`#VO2Jj4 Finished`);
   })
   .catch((error) => {
     console.error(`#Fdisfb Fatal error`);
