90b3cd9de1ab3539a6f28f9bd2eeb73b26ea2cd8
Callum Macdonald <git@callum-macdonald.com>
2022-03-13 17:39:09 +0100

    Some more deps.

--- deps.ts
+++ deps.ts
@@ -2,3 +2,8 @@ export { parse } from "https://deno.land/std@0.129.0/flags/mod.ts";
 export { walk } from "https://deno.land/std@0.129.0/fs/mod.ts";
 export { join } from "https://deno.land/std@0.129.0/path/mod.ts";
 export * as log from "https://deno.land/std@0.129.0/log/mod.ts";
+export { difference } from "https://deno.land/std@0.129.0/datetime/mod.ts";
+import configDir from "https://deno.land/x/config_dir@v0.1.1/mod.ts";
+export { configDir };
+export * as YAML from "https://deno.land/std@0.129.0/encoding/yaml.ts";
+export * as z from "https://deno.land/x/zod@v3.13.4/mod.ts";
